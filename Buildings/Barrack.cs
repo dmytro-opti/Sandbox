ca748bda233761a07c1832ddb3d831a6ed9ff6e8
FrostBan <kaktys132900@gmail.com>
2025-01-18 21:12:50 +0200

    new commit

--- Buildings/Barrack.cs
+++ Buildings/Barrack.cs
@@ -1,10 +1,111 @@
-﻿namespace Sandbox1.Buildings
-{
-    internal class Barrack : GovermentBuilding
-    {
-        public Barrack(string address, int buildingPrice) : base(address, buildingPrice)
-        {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sandbox1.People;
 
+namespace Sandbox1.Buildings
+{
+    public class Barrack : GovermentBuilding
+    {
+        public List<Warrior> Warriors { get; set; } = new List<Warrior>();
+
+        public int MaxWarriorCapacity { get; set; } = 50;
+
+        public Barrack(string address, int buildingPrice) : base(address, buildingPrice) { }
+
+        public void AddWarrior(Warrior warrior)
+        {
+            if (Warriors.Count < MaxWarriorCapacity)
+            {
+                Warriors.Add(warrior);
+            }
+        }
+
+        public void RemoveWarrior(Warrior warrior)
+        {
+            Warriors.Remove(warrior);
+        }
+
+        public void UpdateWarriorRanks()
+        {
+            foreach (var warrior in Warriors.ToList())
+            {
+                if (warrior.Level >= 30)
+                {
+                    warrior.Rank = WarriorRank.Legend; // Перевести в легенди
+                }
+                else if (warrior.Level >= 20)
+                {
+                    warrior.Rank = WarriorRank.Mentor; // Перевести в наставники
+                }
+                else
+                {
+                    warrior.Rank = WarriorRank.Trainee; // Перевести в новобранці
+                }
+            }
+        }
+
+        public void TrainWarriors()
+        {
+            var trainees = Warriors.Where(w => w.Rank == WarriorRank.Trainee).ToList();
+
+            if (trainees.Count == 0)
+            {
+                Console.WriteLine("Немає новобранців для тренування.");
+                return;
+            }
+
+            foreach (var trainee in trainees)
+            {
+                trainee.Train();
+            }
+        }
+
+        public void UpgradeMentors()
+        {
+            var mentors = Warriors.Where(w => w.Rank == WarriorRank.Mentor).ToList();
+
+            foreach (var mentor in mentors)
+            {
+                mentor.UpgradeLevel();
+            }
+        }
+
+        public void CheckHealthAndDeaths()
+        {
+            Random rand = new Random();
+
+            foreach (var warrior in Warriors.ToList())
+            {
+                int randomChance = rand.Next(1, 100);
+                if (randomChance <= (warrior.Rank == WarriorRank.Legend ? 1 : warrior.Rank == WarriorRank.Mentor ? 3 : 5))
+                {
+                    warrior.ReduceHealth(rand.Next(10, 30));
+                }
+            }
+        }
+
+        public void ChangeWarriorEquipment(Warrior warrior, string newWeapon, string newArmor)
+        {
+            if (Warriors.Contains(warrior))
+            {
+                warrior.ChangeEquipment(newWeapon, newArmor);
+            }
+        }
+
+        public List<Warrior> GetTrainees()
+        {
+            return Warriors.Where(w => w.Rank == WarriorRank.Trainee).ToList();
+        }
+
+        public List<Warrior> GetMentors()
+        {
+            return Warriors.Where(w => w.Rank == WarriorRank.Mentor).ToList();
+        }
+
+        public List<Warrior> GetLegends()
+        {
+            return Warriors.Where(w => w.Rank == WarriorRank.Legend).ToList();
         }
     }
 }
--- People/Warrior.cs
+++ People/Warrior.cs
@@ -1,10 +1,110 @@
-﻿namespace Sandbox1.People
-{
-    internal class Warrior : Person
-    {
-        public Warrior()
-        {
+﻿using System;
 
+namespace Sandbox1.People
+{
+    public enum WeaponType
+    {
+        Sword,
+        Spear,
+        Bow,
+        Axe
+    }
+
+    public enum WarriorRank
+    {
+        Trainee,
+        Mentor,
+        Legend
+    }
+
+    public class Warrior : Person
+    {
+        public int Strength { get; set; }
+        public int Agility { get; set; }
+        public int Level { get; set; }
+        public WeaponType Weapon { get; set; }
+        public string Armor { get; set; }
+        public int Health { get; set; }
+        public WarriorRank Rank { get; set; }
+        public WarriorRank CurrentRank => DetermineRank();
+
+        public Warrior(string name, int priority, int monthlySalary, int budget, int strength, int agility, int level, WeaponType weapon, string armor, int health = 100)
+            : base()
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Priority = priority;
+            MonthlySalary = monthlySalary;
+            Budget = budget;
+            Strength = strength;
+            Level = level;
+            Weapon = weapon;
+            Armor = armor;
+            Health = health;
+            Agility = agility;
+        }
+
+        public void Train()
+        {
+            Strength += 5;
+            Agility += 5;
+        }
+
+        public void UpgradeLevel()
+        {
+            Level += 1;
+            Strength += 10;
+            Agility += 10;
+        }
+
+        public void ChangeEquipment(string newWeapon, string newArmor)
+        {
+            try
+            {
+                Weapon = (WeaponType)Enum.Parse(typeof(WeaponType), newWeapon, true);
+                Armor = newArmor;
+                Console.WriteLine($"{Name} змінив спорядження. Нова зброя: {Weapon}, нова броня: {Armor}");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"Невідома зброя: {newWeapon}. Зміна не відбулася.");
+            }
+        }
+
+        public int CalculateDamage()
+        {
+            return Weapon switch
+            {
+                WeaponType.Sword => 15 + (Agility + Level + Strength) / 3,
+                WeaponType.Spear => 20 + (Agility + Level) / 2,
+                WeaponType.Bow => 10 + Agility / 2,
+                WeaponType.Axe => 25 + Strength + Level,
+                _ => 5
+            };
+        }
+
+        public void ReduceHealth(int damage)
+        {
+            Health -= damage;
+            if (Health <= 0)
+            {
+                Console.WriteLine($"{Name} помер у битві!");
+                Health = 0;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} отримав {damage} ушкоджень. Залишилось {Health} здоров'я.");
+            }
+        }
+
+        private WarriorRank DetermineRank()
+        {
+            return Level switch
+            {
+                <= 20 => WarriorRank.Trainee,
+                > 20 and <= 30 => WarriorRank.Mentor,
+                _ => WarriorRank.Legend
+            };
         }
     }
 }
